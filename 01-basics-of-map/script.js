8950d9e89c83c65c8e62f58d16a050181d1fee3c
kunxin-chor <chorkunxin@yahoo.com>
2021-02-01 08:21:53 +0000

    Aded labe 1

---
+++ 01-basics-of-map/script.js
@@ -0,0 +1,13 @@
+let singapore = [1.29, 103.85];
+let map = L.map('singapore-map');
+map.setView(singapore, 13);
+
+// setup tilelayer
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZXh0cmFrdW4iLCJhIjoiY2swdnZtMWVvMTAxaDNtcDVmOHp2c2lxbSJ9.4WxdONppGpMXeHO6rq5xvg'
+}).addTo(map);
